a8883453231fc19673ecc1cb05298a66c134b7cd
yacine-boujelbenne <yacineboujelbenne@gmail.com>
2026-03-23 14:37:00 +0100

    fix(user-service): correct users route import paths

--- userservie/routes/users.js
+++ userservie/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const User = require('../../models/User');
-const { verifyToken } = require('../../middleware/auth');
+const User = require('../models/User');
+const { verifyToken } = require('../middleware/auth');
 
 const router = express.Router();
 
